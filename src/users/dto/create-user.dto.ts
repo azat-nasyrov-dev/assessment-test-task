3fec110fc7af0e2ed102c80c58905c5ad015a367
Azat Nasyrov <azat.nasyrov31@gmail.com>
2024-08-29 20:57:07 +0600

    added dto with validation

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,11 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  readonly name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  readonly email: string;
+}
